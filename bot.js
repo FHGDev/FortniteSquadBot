8c5b20e640964aea6e8b940c6cc43c8dff7858c8
FHGDev <gideon.lingle14@gmail.com>
2018-03-29 21:19:15 -0500

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+// Discord Init
+
+const discord = require('discord.js');
+const bot = new discord.Client();
+
+
+// Event Handler...
+
+bot.on('ready', () => {require('./events/readyEvent.js')(bot)})
+bot.on('message', message => require('./events/messageEvent.js')(bot, message))
+bot.on("error", err => console.error(err))
+bot.on('guildMemberAdd', member => require('./events/guildMemberAdd.js')(bot, member))
+bot.on('guildMemberRemove', member => require('./events/guildMemberRemove.js')(bot, member))
+
+// Bot Made by FHGDev#6545 for the purposes of 'Fortnite Squad' Discord Server.

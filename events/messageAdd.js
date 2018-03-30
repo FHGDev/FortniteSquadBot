23370418bdbb13a45499e71584effbf98466b0df
FHGDev <gideon.lingle14@gmail.com>
2018-03-30 10:02:54 -0500

    Update messageAdd.js

--- events/messageAdd.js
+++ events/messageAdd.js
@@ -1,10 +1,17 @@
 let prefix = "fs!"
+let request = require('snekfetch')
+let discord = require('discord.js')
 
 module.exports = (bot, message) => {
   if (message.content == prefix+"help") {
     message.channel.send(`No commands yet, FHGDev is still working to get me up and functional.`)
   }
   if (message.content == prefix+"meme") {
-    return;
+    request.get('api.imgflip.com/get_memes').then(json => {
+      let meme = json.body.data.memes.url
+      let embed = new discord.RichEmbed()
+      .setImage(meme)
+      message.channel.send({ embed })
+    })
   }
 }

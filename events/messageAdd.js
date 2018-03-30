1708d708b73fb2a66e32e60147aaa29f0f054173
FHGDev <gideon.lingle14@gmail.com>
2018-03-29 21:56:21 -0500

    Create messageAdd.js

---
+++ events/messageAdd.js
@@ -0,0 +1,8 @@
+module.exports = (bot, message) => {
+  if (message.content == "fs!help") {
+    message.channel.send(`No commands yet, FHGDev is still working to get me up and functional.`)
+  }
+  if (message.content == "fs!meme") {
+    return;
+  }
+}
